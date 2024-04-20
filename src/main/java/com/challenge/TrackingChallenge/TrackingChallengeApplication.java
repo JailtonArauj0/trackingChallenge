268f81f751016bc664cdf95f2bf6be89dfa21981
Jailton <jailton.flames@gmail.com>
2024-04-20 10:11:42 -0300

    Projeto inicial

---
+++ src/main/java/com/challenge/TrackingChallenge/TrackingChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.TrackingChallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackingChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackingChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/challenge/TrackingChallenge/TrackingChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.TrackingChallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackingChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
